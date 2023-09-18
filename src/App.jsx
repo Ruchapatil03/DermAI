f60d1a827fd0b784744db4bdf17f09abf81b415e
Ruchapatil03 <rucha14patil@gmail.com>
2023-09-18 19:08:08 +0530

    added login and signup route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Signup from "./pages/SignUp";
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/Login" element={<Login/>}></Route>
+          <Route path="/Signup" element={<Signup/>}></Route>
         </Routes>
       </Router>
     </>
