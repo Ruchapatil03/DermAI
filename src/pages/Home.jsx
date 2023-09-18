f5f97170b89242a04c75f85b77e084b2d942aaf1
Ruchapatil03 <rucha14patil@gmail.com>
2023-09-18 13:46:00 +0530

    Home fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,10 @@
-import React, { useEffect, useState } from 'react'
-
-import '../stylesheets/Home.css'
-
-import ParticleEffectButton from 'react-particle-effect-button'
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
 
 const Home = () => {
-
-    const [hidden , sethidden] = useState(true)
-
-    useEffect(() =>{
-        sethidden(false)
-    },[])
-
-    return (
-        <ParticleEffectButton
-        color='#FF0000'
-        hidden={hidden}
-        duration={2000}>
-            <div className="container">
-                Home
-            </div>
-        </ParticleEffectButton>
-    )
+  return (
+    <div>Home</div>
+  )
 }
 
 export default Home
