4e09066370b1d9fd740403e36e93fc601cd570d7
Ruchapatil03 <rucha14patil@gmail.com>
2023-09-20 01:50:13 +0530

    Quick fix in login n signup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import "../stylesheets/Home.css";
-
+import bg from "../assets/Home/bg.svg";
 const Home = () => {
   return (
     <>
-    <div className="sec1">
-      Home
+    <div className="section1">
+      <img src={bg} alt="" />
     </div>
     </>
   );
