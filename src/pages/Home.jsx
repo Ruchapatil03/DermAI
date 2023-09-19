6d3b4afc89e7b6b425de066117e646f3eeb41a4a
Ruchapatil03 <rucha14patil@gmail.com>
2023-09-20 04:11:41 +0530

    Home page design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,28 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import "../stylesheets/Home.css";
-import bg from "../assets/Home/bg.svg";
+// import bg from "../assets/Home/gradbg1.svg";
+import bg from "../assets/Home/newbg.png";
+
 const Home = () => {
   return (
     <>
+    <title>DermAI</title>
     <div className="secy1">
-      <div className='heading'>
-      <h1>DERMAI</h1>
-      </div>
-     
+      <section className="homey">
+        <div className="bgimg">
+          <img src={bg} alt="" />
+          <div className="darkenimage"></div>
+          <div className="content">
+            <span className="head">DERM-AI</span>   
+            {/* <img src={ok} alt="" /> */}
+          </div>
+   
+        </div>
+      </section>
     </div>
-      <div>
-      <img src={bg} alt="" />
-      </div>
+    
+    
     </>
   );
 }
