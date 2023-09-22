d236532f55eb2d596facb1852f7e3cc1e51dcb48
Unmani Shinde <mailto.unmani@gmail.com>
2023-09-22 12:04:10 +0530

    Security Updates

--- src/RPC/network.js
+++ src/RPC/network.js
@@ -4,7 +4,7 @@ import ContractABI from '../ABI/RevisedABI.json';
 
 export const magicProvider = new Magic('pk_live_458EA07ABE8ADAEA', {
     network: {
-      rpcUrl: 'https://sepolia.infura.io/v3/cc5bd360c5e7491888fe47af0fdc1584',
+      rpcUrl: 'https://sepolia.infura.io/v3/YOUR_PROJECT_ID',
       chainId: 11155111,
     }, // or your own custom node url
   });
@@ -13,4 +13,3 @@ const provider = await magicProvider.wallet.getProvider();
 export const contractAddress = '0x6E6FD340FD7BE37e06888824f9F13CC010A93D12';
 export const web3 = new Web3(provider);
 export const contractInstance = new web3.eth.Contract(ContractABI, contractAddress);
-
